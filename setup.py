61b4918043b9919610014e674a6007db25333f06
Jesse B Miller <jmiller@adops.com>
2014-09-19 11:19:19 -0500

    adds setup.py

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name = 'hellolib',
+    packages = ['hellolib'],
+    version = '0.0.1',
+    description = 'a hello function',
+    author = 'Jesse B Miller',
+    author_email = 'jmiller@adops.com',
+    classifiers = [],
+    url = 'https://github.com/OAODEV/hellolib',
+)
